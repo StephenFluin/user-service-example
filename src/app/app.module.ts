30a90eb132a15be6016dc24fc7a0efc6eed598ef
Stephen Fluin <stephen.fluin@gmail.com>
2018-08-28 05:25:22 -0700

    create AngularFire user service with RBAC

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [AppComponent],
+    imports: [
+        BrowserModule,
+        AngularFireModule.initializeApp({
+            apiKey: 'AIzaSyAlSVvAzNRjUdJmzc5kHZWZnqH7T2WIJu4',
+            authDomain: 'user-service-example-51951.firebaseapp.com',
+            databaseURL: 'https://user-service-example-51951.firebaseio.com',
+            projectId: 'user-service-example-51951',
+            storageBucket: '',
+            messagingSenderId: '36155795813',
+        }),
+        AngularFireAuthModule,
+        AngularFireDatabaseModule,
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
